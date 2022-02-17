e6c4eb7331d4c4b306d24da231bbca0b7221f647
Jone Lura <jone.lura@nav.no>
2022-02-17 20:42:30 +0100

    debug appendHeaders

--- server/middleware/headers.ts
+++ server/middleware/headers.ts
@@ -4,6 +4,9 @@ import { NextFunction, Request, Response } from 'express';
 export const appendHeaders = async (req: Request, res: Response, next: NextFunction) => {
   const tokenSet = await exchangeToken(req);
 
+  // fjern eksisterende fra request
+  delete req.headers['authorization'];
+
   if (!tokenSet?.expired() && tokenSet?.access_token) {
     req.headers['Authorization'] = `Bearer ${tokenSet.access_token}`;
   }
