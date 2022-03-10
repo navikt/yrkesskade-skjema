69a4d84b4387e0a75aa5f06488d35d30810fc736
Jone Lura <jone.lura@gmail.com>
2022-03-10 17:33:12 +0100

    fix adresse

--- client/src/pages/Form/Accident/index.tsx
+++ client/src/pages/Form/Accident/index.tsx
@@ -27,12 +27,10 @@ const AccidentFormPage = () => {
     formState: { errors },
   } = useForm({
     defaultValues: {
-      'hendelsesfakta.ulykkessted.sammeSomVirksomhetensAdresse': state.hendelsesfakta.ulykkessted.sammeSomVirksomhetensAdresse,
-      'hendelsesfakta.ulykkessted.adresse.adresselinje1': state.hendelsesfakta.ulykkessted.adresse?.adresselinje1,
-      'hendelsesfakta.ulykkessted.adresse.adresselinje2': state.hendelsesfakta.ulykkessted.adresse?.adresselinje2,
+      'hendelsesfakta.ulykkessted.adresse.adresselinje1': state.hendelsesfakta.ulykkessted.adresse?.adresselinje1 || '',
+      'hendelsesfakta.ulykkessted.adresse.adresselinje2': state.hendelsesfakta.ulykkessted.adresse?.adresselinje2 || '',
       'hendelsesfakta.ulykkessted.adresse.adresselinje3': state.hendelsesfakta.ulykkessted.adresse?.adresselinje3,
-      'hendelsesfakta.ulykkessted.adresse.land': state.hendelsesfakta.ulykkessted.adresse?.land,
-      'skade.alvorlighetsgrad': state.skade.alvorlighetsgrad,
+      'hendelsesfakta.ulykkessted.adresse.land': state.hendelsesfakta.ulykkessted.adresse?.land || '',
       'hendelsesfakta.hvorSkjeddeUlykken': state.hendelsesfakta.hvorSkjeddeUlykken,
       'hendelsesfakta.stedsbeskrivelseTabellF': state.hendelsesfakta.stedsbeskrivelseTabellF,
     }
