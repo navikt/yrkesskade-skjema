f833f04f360861fb9a093620849bebe8477fca7e
Jarand Bjerkhaug <jarand.bjerkhaug@gmail.com>
2022-03-16 10:53:27 +0100

    added a mask to time field

--- client/src/components/Forms/Timeframe/index.tsx
+++ client/src/components/Forms/Timeframe/index.tsx
@@ -2,8 +2,9 @@ import { useState, useEffect, useRef } from 'react';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
-import { Select, RadioGroup, Label, TextField } from '@navikt/ds-react';
+import { Select, RadioGroup, Label } from '@navikt/ds-react';
 import { useStateMachine } from 'little-state-machine';
+import InputMask from 'react-input-mask';
 
 import ulykkestid from '../../../assets/Lists/ulykkestid';
 
@@ -30,20 +31,23 @@ const TimeframeForm = ({ register, errors, control, setValue }: IProps) => {
   const dayPickerClassNames = {
     container: 'nav-day-picker',
     overlay: '',
-    overlayWrapper: ''
+    overlayWrapper: '',
   } as InputClassNames;
 
-  const whenDayPickerClassNames = { ...dayPickerClassNames,
-    container: `timeframe-when-date ${dayPickerClassNames.container}`
-  }
+  const whenDayPickerClassNames = {
+    ...dayPickerClassNames,
+    container: `timeframe-when-date ${dayPickerClassNames.container}`,
+  };
 
-  const fromDayPickerClassNames = { ...dayPickerClassNames,
-    container: `timeframe-from-date ${dayPickerClassNames.container}`
-  }
+  const fromDayPickerClassNames = {
+    ...dayPickerClassNames,
+    container: `timeframe-from-date ${dayPickerClassNames.container}`,
+  };
 
-  const toDayPickerClassNames = { ...dayPickerClassNames,
-    container: `timeframe-from-date ${dayPickerClassNames.container}`
-  }
+  const toDayPickerClassNames = {
+    ...dayPickerClassNames,
+    container: `timeframe-from-date ${dayPickerClassNames.container}`,
+  };
 
   const [timeType, setTimeType] = useState(state.hendelsesfakta.tid.tidstype);
   const [specificDate, setSpecificDate] = useState<Date | undefined>(
@@ -52,17 +56,17 @@ const TimeframeForm = ({ register, errors, control, setValue }: IProps) => {
 
   const [specificTime, setSpecificTime] = useState<string | undefined>(
     handleTimeValue(state.hendelsesfakta.tid.tidspunkt)
-  )
+  );
 
-  const [ toDayInput, setToDayInput ] = useState<DayPickerInput | null>();
-  const [ specificFromDay, setSpecificFromDay ] = useState<Date | undefined>(
+  const [toDayInput, setToDayInput] = useState<DayPickerInput | null>();
+  const [specificFromDay, setSpecificFromDay] = useState<Date | undefined>(
     handleDateValue(state.hendelsesfakta.tid.periode.fra)
   );
   const [specificToDay, setSpecificToDay] = useState<Date | undefined>(
     handleDateValue(state.hendelsesfakta.tid.periode.til)
   );
 
-  const modifiers = { start: specificFromDay, end: specificToDay};
+  const modifiers = { start: specificFromDay, end: specificToDay };
 
   const handleSpecificDate = (selectedDay: Date) => {
     setSpecificDate(selectedDay);
@@ -70,28 +74,29 @@ const TimeframeForm = ({ register, errors, control, setValue }: IProps) => {
 
   const handleSpecificFromDay = (selectedDay: Date) => {
     setSpecificFromDay(selectedDay);
-  }
+  };
 
   const handleSpecificToDay = (selectedDay: Date) => {
     setSpecificToDay(selectedDay);
-  }
+  };
 
   const handleKlokkeChange = (event: any) => {
     setSpecificTime(event.target.value);
-  }
+  };
 
   useEffect(() => {
     if (specificDate && specificTime && specificTime.length === 5) {
       const timeparts = specificTime.split(':');
       const newDate = new Date(
         specificDate.getUTCFullYear(),
-        specificDate?.getUTCMonth(), specificDate?.getUTCDay(),
+        specificDate?.getUTCMonth(),
+        specificDate?.getUTCDay(),
         parseInt(timeparts[0]),
-        parseInt(timeparts[1]));
+        parseInt(timeparts[1])
+      );
 
-        setValue('hendelsesfakta.tid.tidspunkt', newDate);
+      setValue('hendelsesfakta.tid.tidspunkt', newDate);
     }
-
   }, [specificTime, specificDate, setValue]);
 
   useEffect(() => {
@@ -117,7 +122,7 @@ const TimeframeForm = ({ register, errors, control, setValue }: IProps) => {
 
     // ikke dato
     return undefined;
-  }
+  };
 
   const specificDateError = useRef('');
   let specificRangeError = '';
@@ -172,7 +177,7 @@ const TimeframeForm = ({ register, errors, control, setValue }: IProps) => {
               }}
               render={({ field: { onChange, onBlur, value, name, ref } }) => (
                 <DayPickerInput
-                  classNames={ { ...whenDayPickerClassNames } }
+                  classNames={{ ...whenDayPickerClassNames }}
                   placeholder=""
                   value={specificDate}
                   onDayChange={handleSpecificDate}
@@ -196,20 +201,26 @@ const TimeframeForm = ({ register, errors, control, setValue }: IProps) => {
           </div>
         )}
 
-          {timeType === 'Tidspunkt' && specificDate !== null && (
-                <TextField
-                  label="Tid for ulykken"
-                      className="spacer"
-                      onChange={handleKlokkeChange}
-                      value={specificTime || ''}
-                      error={
-                        errors?.hendelsesfakta?.tid?.tidspunktTime &&
-                        errors?.hendelsesfakta?.tid?.tidspunktTime.message
-                      }
-                      data-testid="timeframe-when-time"
-                    />
-
-            )}
+        {timeType === 'Tidspunkt' && specificDate !== null && (
+          <div className="spacer">
+            <label htmlFor="timeframe-when-time" className="navds-label">
+              Tid for ulykken
+            </label>
+            <InputMask
+              mask="99:99"
+              onChange={handleKlokkeChange}
+              value={specificTime || ''}
+              data-testid="timeframe-when-time"
+              id="timeframe-when-time"
+              className="navds-text-field__input navds-body-short navds-body-medium"
+            />
+             {errors?.hendelsesfakta?.tid?.tidspunktTime && (
+                  <span className="navds-error-message navds-error-message--medium navds-label">
+                    {errors?.hendelsesfakta?.tid?.tidspunktTime.message}
+                  </span>
+                )}
+          </div>
+        )}
 
         <div className="navds-radio navds-radio--medium">
           <input
@@ -234,45 +245,45 @@ const TimeframeForm = ({ register, errors, control, setValue }: IProps) => {
           {timeType === 'Periode' && (
             <div className="periode-container spacer">
               <div>
-              <Label>Fra dag</Label>
-              <DayPickerInput
-                classNames={ fromDayPickerClassNames }
-                placeholder=""
-                value={specificFromDay}
-                onDayChange={handleSpecificFromDay}
-                formatDate={formatDate}
-                format={FORMAT}
-                parseDate={parseDate}
-                dayPickerProps={{
-                  toMonth: specificToDay,
-                  disabledDays: {
-                    after: new Date(),
-                  },
-                  modifiers,
-                  onDayClick: () => (toDayInput?.getInput().focus()),
-                }}
-              />
+                <Label>Fra dag</Label>
+                <DayPickerInput
+                  classNames={fromDayPickerClassNames}
+                  placeholder=""
+                  value={specificFromDay}
+                  onDayChange={handleSpecificFromDay}
+                  formatDate={formatDate}
+                  format={FORMAT}
+                  parseDate={parseDate}
+                  dayPickerProps={{
+                    toMonth: specificToDay,
+                    disabledDays: {
+                      after: new Date(),
+                    },
+                    modifiers,
+                    onDayClick: () => toDayInput?.getInput().focus(),
+                  }}
+                />
               </div>
               <div>
-              <Label>Til dag</Label>
-              <DayPickerInput
-                ref={ el => (setToDayInput(el))}
-                classNames={ toDayPickerClassNames }
-                placeholder=""
-                value={specificToDay}
-                onDayChange={handleSpecificToDay}
-                formatDate={formatDate}
-                format={FORMAT}
-                parseDate={parseDate}
-                dayPickerProps={{
-                  month: specificFromDay,
-                  fromMonth: specificFromDay,
-                  modifiers,
-                  disabledDays: {
-                    after: new Date(),
-                  },
-                }}
-              />
+                <Label>Til dag</Label>
+                <DayPickerInput
+                  ref={(el) => setToDayInput(el)}
+                  classNames={toDayPickerClassNames}
+                  placeholder=""
+                  value={specificToDay}
+                  onDayChange={handleSpecificToDay}
+                  formatDate={formatDate}
+                  format={FORMAT}
+                  parseDate={parseDate}
+                  dayPickerProps={{
+                    month: specificFromDay,
+                    fromMonth: specificFromDay,
+                    modifiers,
+                    disabledDays: {
+                      after: new Date(),
+                    },
+                  }}
+                />
               </div>
 
               {specificRangeError?.length > 0 && (
@@ -331,223 +342,3 @@ const TimeframeForm = ({ register, errors, control, setValue }: IProps) => {
   );
 };
 export default TimeframeForm;
-
-// import { useState } from 'react';
-// import { Select, RadioGroup, Label } from '@navikt/ds-react';
-// import DatePicker, { registerLocale } from 'react-datepicker';
-// import { useStateMachine } from 'little-state-machine';
-// import nb from 'date-fns/locale/nb';
-// // import { path } from 'ramda';
-// import { get } from 'lodash';
-
-// import './timeframeForm.less';
-// import ulykkestid from '../../../assets/Lists/ulykkestid';
-// import { parseISO } from 'date-fns';
-
-// registerLocale('nb', nb);
-// interface IProps {
-//   register: any;
-//   errors: any;
-//   setValue: any;
-// }
-// const TimeframeForm = ({ register, errors, setValue }: IProps) => {
-//   const { state } = useStateMachine();
-//   const [timeType, setTimeType] = useState(state.hendelsesfakta.tid.tidstype);
-
-//   // const [specificDate, setSpecificDate] = useState<Date | null>(
-//   //   typeof path(['hendelsesfakta', 'tid', 'tidspunkt'], state) === 'string'
-//   //     ? parseISO(state.hendelsesfakta.tid.tidspunkt + '')
-//   //     : null
-//   // );
-//     // const [specificDate, setSpecificDate] = useState<Date | undefined>(
-//     const [specificDate, setSpecificDate] = useState<any>(
-//     get(state, 'hendelsesfakta.tid.tidspunkt')
-//   );
-//   // const [startDateRange, setStartDateRange] = useState<Date | null>(
-//   //   typeof path(['hendelsesfakta', 'tid', 'periode', 'fra'], state) === 'string'
-//   //     ? parseISO(path(['hendelsesfakta', 'tid', 'periode', 'fra'], state) + '')
-//   //     : null
-//   // );
-//   // const [endDateRange, setEndDateRange] = useState<Date | null>(
-//   //   typeof path(['hendelsesfakta', 'tid', 'periode', 'til'], state) === 'string'
-//   //     ? parseISO(path(['hendelsesfakta', 'tid', 'periode', 'til'], state) + '')
-//   //     : null
-//   // );
-
-//   // const onChangeRange = (dates: (Date | null)[]) => {
-//   //   const [startRange, endRange] = dates;
-//   //   setStartDateRange(startRange);
-//   //   setValue('hendelsesfakta.tid.periode.fra', startRange?.toISOString());
-//   //   setValue('hendelsesfakta.tid.periode.til', endRange?.toString());
-//   //   setEndDateRange(endRange);
-//   // };
-
-//   const onChangeSpecificDate = (date: any) => {
-//     console.log(date);
-//     setSpecificDate(date);
-//     setValue('hendelsesfakta.tid.tidspunkt', date?.toISOString());
-//   };
-//   return (
-//     <>
-//       <RadioGroup
-//         legend="Når skjedde ulykken som skal meldes?"
-//         error={
-//           errors?.hendelsesfakta?.tid?.tidstype &&
-//           errors?.hendelsesfakta?.tid?.tidstype.message
-//         }
-//       >
-//         <div className="navds-radio navds-radio--medium">
-//           <input
-//             type="radio"
-//             className="navds-radio__input"
-//             {...register('hendelsesfakta.tid.tidstype', {
-//               required: 'Dette feltet er påkrevd',
-//             })}
-//             value="Tidspunkt"
-//             data-testid="timeframe-when-date"
-//             id="timeframe-when-date"
-//             onChange={(e) => {
-//               setTimeType(e.target.value);
-//             }}
-//           />
-//           <label htmlFor="timeframe-when-date" className="navds-radio__label">
-//             På en dato
-//           </label>
-//         </div>
-//         {timeType === 'Tidspunkt' && (
-//           <div>
-//                 <DatePicker
-//                   className="navds-text-field__input navds-body-short navds-body-medium timeframe-when-date"
-//                   onChange={onChangeSpecificDate}
-//                   selected={parseISO(specificDate)}
-//                   maxDate={new Date()}
-//                   locale="nb"
-//                   dateFormat={['dd.MM.yyyy', 'ddMMyyyy', 'ddMMyy']}
-//                   // shouldCloseOnSelect={false}
-//                   data-testid="timeframe-when-date"
-//                 />
-//             {errors?.hendelsesfakta?.tid?.tidspunkt && (
-//               <span className="navds-error-message navds-error-message--medium navds-label">
-//                 {errors?.hendelsesfakta?.tid?.tidspunkt.message}
-//               </span>
-//             )}
-
-//               <div className="spacer">
-//                 <Label>Tid for ulykken</Label>
-//                     <DatePicker
-//                       className="navds-text-field__input navds-body-short navds-body-medium timeframe-when-time"
-//                       onChange={onChangeSpecificDate}
-//                       selected={specificDate}
-//                       maxDate={new Date()}
-//                       locale="nb"
-//                       shouldCloseOnSelect={false}
-//                       showTimeSelect
-//                       showTimeSelectOnly
-//                       timeIntervals={15}
-//                       timeCaption="Time"
-//                       dateFormat="HH:mm"
-//                     />
-//                 {errors?.hendelsesfakta?.tid?.tidspunktTime && (
-//                   <span className="navds-error-message navds-error-message--medium navds-label">
-//                     {errors?.hendelsesfakta?.tid?.tidspunktTime.message}
-//                   </span>
-//                 )}
-//               </div>
-//           </div>
-//         )}
-
-//         <div className="navds-radio navds-radio--medium">
-//           <input
-//             type="radio"
-//             className="navds-radio__input"
-//             {...register('hendelsesfakta.tid.tidstype', {
-//               required: 'Dette feltet er påkrevd',
-//             })}
-//             value="Periode"
-//             data-testid="timeframe-when-over-period"
-//             id="timeframe-when-over-period"
-//             onChange={(e) => {
-//               setTimeType(e.target.value);
-//             }}
-//           />
-//           <label
-//             htmlFor="timeframe-when-over-period"
-//             className="navds-radio__label"
-//           >
-//             Over en periode
-//           </label>
-//         </div>
-
-//         {/* {timeType === 'Periode' && (
-//           <div className="spacer">
-//             <Label>Fra - Til</Label>
-
-//                 <DatePicker
-//                   className="navds-text-field__input navds-body-short navds-body-medium"
-//                   maxDate={new Date()}
-//                   locale="nb"
-//                   dateFormat={['dd.MM.yyyy', 'ddMMyyyy', 'ddMMyy']}
-//                   selectsRange
-//                   selected={startDateRange}
-//                   onChange={onChangeRange}
-//                   startDate={startDateRange}
-//                   endDate={endDateRange}
-//                   shouldCloseOnSelect={false}
-//                 />
-//             {errors?.hendelsesfakta?.tid?.periode && (
-//               <span className="navds-error-message navds-error-message--medium navds-label">
-//                 {errors?.hendelsesfakta?.tid?.periode.message}
-//               </span>
-//             )}
-//           </div>
-//         )} */}
-
-//         <div className="navds-radio navds-radio--medium">
-//           <input
-//             type="radio"
-//             className="navds-radio__input"
-//             {...register('hendelsesfakta.tid.tidstype', {
-//               required: 'Dette feltet er påkrevd',
-//             })}
-//             value="Ukjent"
-//             data-testid="timeframe-when-unknown"
-//             id="timeframe-when-unknown"
-//             onChange={(e) => {
-//               setTimeType(e.target.value);
-//             }}
-//           />
-//           <label
-//             htmlFor="timeframe-when-unknown"
-//             className="navds-radio__label"
-//           >
-//             Ukjent
-//           </label>
-//         </div>
-//       </RadioGroup>
-
-//       <Select
-//         className="spacer"
-//         {...register('hendelsesfakta.naarSkjeddeUlykken', {
-//           required: 'Dette feltet er påkrevd',
-//         })}
-//         error={
-//           errors?.hendelsesfakta?.naarSkjeddeUlykken &&
-//           errors?.hendelsesfakta?.naarSkjeddeUlykken.message
-//         }
-//         label="Innenfor hvilket tidsrom inntraff skaden?"
-//         data-testid="timeframe-period-options"
-//       >
-//         <option hidden value=""></option>
-//         {ulykkestid.map((time: { value: string; label: string }) => {
-//           return (
-//             <option key={encodeURIComponent(time.value)} value={time.value}>
-//               {time.label}
-//             </option>
-//           );
-//         })}
-//       </Select>
-//     </>
-//   );
-// };
-
-// export default TimeframeForm;
