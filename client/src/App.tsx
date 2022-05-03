93714893f4855b46f452eeab736684876acc17e8
Jarand Bjerkhaug <jarand.bjerkhaug@gmail.com>
2022-05-03 14:41:08 +0200

    took down site temporarily

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import NotFound from './pages/404';
-import Info from './pages/Info';
-import Summary from './pages/Summary';
-import Receipt from './pages/Receipt';
-import Error from './pages/Error';
-import TimeframeFormPage from './pages/Form/Timeframe';
-import InjuryFormPage from './pages/Form/Injury';
-import InjuredFormPage from './pages/Form/Injured';
-import AccidentFormPage from './pages/Form/Accident';
-import DescriptionFormPage from './pages/Form/Description';
+// import Info from './pages/Info';
+// import Summary from './pages/Summary';
+// import Receipt from './pages/Receipt';
+// import Error from './pages/Error';
+// import TimeframeFormPage from './pages/Form/Timeframe';
+// import InjuryFormPage from './pages/Form/Injury';
+// import InjuredFormPage from './pages/Form/Injured';
+// import AccidentFormPage from './pages/Form/Accident';
+// import DescriptionFormPage from './pages/Form/Description';
+// import Landing from './pages/Landing';
+import TemporaryDown from './pages/TemporaryDown';
+
 
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { StateMachineProvider, createStore } from 'little-state-machine';
@@ -16,7 +19,6 @@ import { InnloggetProvider } from './context/InnloggetContext';
 import { FeatureTogglesProvider } from './context/FeatureTogglesContext';
 import { autentiseringsInterceptor } from './utils/autentisering';
 import { SelectedCompanyProvider } from './context/SelectedCompanyContext';
-import Landing from './pages/Landing';
 import { ErrorMessageProvider } from './context/ErrorMessageContext';
 import { formState } from './State/formState';
 import { StateManagementProvider } from './context/StateManagementContext';
@@ -43,7 +45,8 @@ const App = () => {
               <StateManagementProvider>
                 <Routes>
                   <Route path="yrkesskade/">
-                    <Route index element={<Landing />} />
+                    <Route index element={<TemporaryDown />} />
+                    {/* <Route index element={<Landing />} />
                     <Route path="skjema">
                       <Route index element={<Info />} />
                       <Route path="skadelidt" element={<InjuredFormPage />} />
@@ -58,7 +61,7 @@ const App = () => {
                       <Route path="kvittering" element={<Receipt />} />
                       <Route path="feilmelding" element={<Error />} />
                     </Route>
-                    <Route path="feilmelding" element={<Error />} />
+                    <Route path="feilmelding" element={<Error />} /> */}
                   </Route>
                   <Route path="*" element={<NotFound />} />
                 </Routes>
