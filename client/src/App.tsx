ad32cab53b3b2ad57ead9df499688e8d36f8a77b
Jone Lura <jone.lura@gmail.com>
2022-05-09 08:33:28 +0200

    YSMOD-281
    - bruker unleash til å sette applikasjonen utligjengelig

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,23 @@
 import NotFound from './pages/404';
-// import Info from './pages/Info';
-// import Summary from './pages/Summary';
-// import Receipt from './pages/Receipt';
-// import Error from './pages/Error';
-// import TimeframeFormPage from './pages/Form/Timeframe';
-// import InjuryFormPage from './pages/Form/Injury';
-// import InjuredFormPage from './pages/Form/Injured';
-// import AccidentFormPage from './pages/Form/Accident';
-// import DescriptionFormPage from './pages/Form/Description';
-// import Landing from './pages/Landing';
-import TemporaryDown from './pages/TemporaryDown';
-
+import Info from './pages/Info';
+import Summary from './pages/Summary';
+import Receipt from './pages/Receipt';
+import Error from './pages/Error';
+import TimeframeFormPage from './pages/Form/Timeframe';
+import InjuryFormPage from './pages/Form/Injury';
+import InjuredFormPage from './pages/Form/Injured';
+import AccidentFormPage from './pages/Form/Accident';
+import DescriptionFormPage from './pages/Form/Description';
+import Landing from './pages/Landing';
 
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { StateMachineProvider, createStore } from 'little-state-machine';
 
 import { InnloggetProvider } from './context/InnloggetContext';
-import { FeatureTogglesProvider } from './context/FeatureTogglesContext';
+import {
+  FeatureTogglesProvider,
+  useFeatureToggles,
+} from './context/FeatureTogglesContext';
 import { autentiseringsInterceptor } from './utils/autentisering';
 import { SelectedCompanyProvider } from './context/SelectedCompanyContext';
 import { ErrorMessageProvider } from './context/ErrorMessageContext';
@@ -24,6 +25,7 @@ import { formState } from './State/formState';
 import { StateManagementProvider } from './context/StateManagementContext';
 import { useEffect } from 'react';
 import { logAmplitudeEvent } from './utils/analytics/amplitude';
+import TemporaryDown from './pages/TemporaryDown';
 
 const App = () => {
   createStore(formState, {});
@@ -31,8 +33,10 @@ const App = () => {
 
   useEffect(() => {
     console.log(location);
-    logAmplitudeEvent('skademelding.sidevisning', { pathname: location.pathname });
-  }, [location])
+    logAmplitudeEvent('skademelding.sidevisning', {
+      pathname: location.pathname,
+    });
+  }, [location]);
 
   autentiseringsInterceptor();
 
@@ -43,34 +47,47 @@ const App = () => {
           <SelectedCompanyProvider>
             <StateMachineProvider>
               <StateManagementProvider>
-                <Routes>
-                  <Route path="yrkesskade/">
-                    <Route index element={<TemporaryDown />} />
-                    {/* <Route index element={<Landing />} />
-                    <Route path="skjema">
-                      <Route index element={<Info />} />
-                      <Route path="skadelidt" element={<InjuredFormPage />} />
-                      <Route path="tidsrom" element={<TimeframeFormPage />} />
-                      <Route path="ulykken" element={<AccidentFormPage />} />
-                      <Route path="skaden" element={<InjuryFormPage />} />
-                      <Route
-                        path="beskrivelse"
-                        element={<DescriptionFormPage />}
-                      />
-                      <Route path="oppsummering" element={<Summary />} />
-                      <Route path="kvittering" element={<Receipt />} />
-                      <Route path="feilmelding" element={<Error />} />
-                    </Route>
-                    <Route path="feilmelding" element={<Error />} /> */}
-                  </Route>
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
+                <AppContent />
               </StateManagementProvider>
             </StateMachineProvider>
           </SelectedCompanyProvider>
         </FeatureTogglesProvider>
       </InnloggetProvider>
     </ErrorMessageProvider>
+  );
+};
+
+const AppContent = () => {
+  const { toggles } = useFeatureToggles();
+
+  return (
+    <Routes>
+      <Route path="yrkesskade/">
+        {toggles && !toggles.SKADEMELDING_TILGJENGELIG ? (
+          <>
+          <Route index element={<TemporaryDown />} />
+          <Route path="*" element={<TemporaryDown />} />
+          </>
+        ) : (
+          <>
+            <Route index element={<Landing />} />
+            <Route path="skjema">
+              <Route index element={<Info />} />
+              <Route path="skadelidt" element={<InjuredFormPage />} />
+              <Route path="tidsrom" element={<TimeframeFormPage />} />
+              <Route path="ulykken" element={<AccidentFormPage />} />
+              <Route path="skaden" element={<InjuryFormPage />} />
+              <Route path="beskrivelse" element={<DescriptionFormPage />} />
+              <Route path="oppsummering" element={<Summary />} />
+              <Route path="kvittering" element={<Receipt />} />
+              <Route path="feilmelding" element={<Error />} />
+            </Route>
+
+          </>
+        )}
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
 
