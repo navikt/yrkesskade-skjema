1678ab6063c6899580677bc0222d4e91fa929749
Jone Lura <jone.lura@gmail.com>
2022-06-24 12:32:33 +0200

    YSMOD-302
    - fjernet flere ubrukte variabler

--- client/src/components/Forms/Timeframe/index.tsx
+++ client/src/components/Forms/Timeframe/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
